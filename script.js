3138c6a162dac01f235d29a0cf8cc43f4894d1d3
yash <hsay0605@gmail.com>
2020-09-26 12:28:38 +0530

    Removed redundant code

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const DARK_THEME = 'dark';
 const LIGHT_THEME = 'light';
+const TITLE_HOME_DARK = 'Welcome to the Dark side';
+const TITLE_HOME_LIGHT = 'Welcome to the Light';
+const rootElement = document.documentElement;
 
 const toggleButton = document.querySelector('input[type=checkbox]');
-const nav = document.getElementById('nav');
-const textBox = document.getElementById('text-box');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
-const toggleIcon = document.getElementById('toggle-icon');
-const titleText = document.getElementById('title-text');
+const nav = setVariables('nav');
+const textBox = setVariables('text-box');
+const image1 = setVariables('image1');
+const image2 = setVariables('image2');
+const image3 = setVariables('image3');
+const toggleIcon = setVariables('toggle-icon');
+const titleText = setVariables('title-text');
 
-// function setVariables(key, value) {
-//     key = 
-// }
+function setVariables(value) {
+    return document.getElementById(value)
+}
 
-const rootElement = document.documentElement;
+function setImage() {}
 
 function switchColor(isDark) {
     const mode = isDark === DARK_THEME ? true : false;
+
+    rootElement.setAttribute('data-theme', isDark);
+    localStorage.setItem('theme', isDark);
+
     nav.style.backgroundColor = mode ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = mode ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
 
@@ -25,33 +32,24 @@ function switchColor(isDark) {
     let removeClass = !mode ? 'fa-moon' : 'fa-sun';
     let addClass = mode ? 'fa-moon' : 'fa-sun';
     toggleIcon.children[1].classList.replace(removeClass, addClass);    
-    titleText.textContent = mode ? 'Welcome to the Dark side' : 'Welcome to the Light';
+    titleText.textContent = mode ? TITLE_HOME_DARK : TITLE_HOME_LIGHT;
 
-    let imageColor = mode ? 'dark' : 'light';
+    let imageColor = mode ? DARK_THEME : LIGHT_THEME;
+    
     image1.src =`img/undraw_conceptual_idea_${imageColor}.svg`;
     image2.src =`img/undraw_feeling_proud_${imageColor}.svg`;
     image3.src =`img/undraw_proud_coder_${imageColor}.svg`;
 }
 
 toggleButton.addEventListener('change', (event) => {
-    if(event.target.checked) {
-        rootElement.setAttribute('data-theme', DARK_THEME)
-        localStorage.setItem('theme', DARK_THEME)
-    } else {
-        rootElement.setAttribute('data-theme', LIGHT_THEME)
-        localStorage.setItem('theme', LIGHT_THEME)
-    }
     switchColor(event.target.checked ? DARK_THEME : LIGHT_THEME);
 });
 
 const currentMode = localStorage.getItem('theme');
 
 if(currentMode) {
-    let modeVal = currentMode === DARK_THEME ? true : false;
-    rootElement.setAttribute('data-theme', currentMode);
-    switchColor(modeVal);
-
-    if(modeVal) {
+    switchColor(currentMode);
+    if(currentMode === DARK_THEME) {
         toggleButton.checked = true;
     }
 } else {
